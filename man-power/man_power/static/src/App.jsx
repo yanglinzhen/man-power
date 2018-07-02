31eed9bab272a36245ec01a4242ff034ac9cdb75
yanglinzhen <yanglinzhen@outlook.com>
2018-07-02 22:18:05 +0800

    add react-router

--- man-power/man_power/static/src/App.jsx
+++ man-power/man_power/static/src/App.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Nav,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 import axios from './AxiosConfig.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './Home.jsx'
 
 class App extends Component {
   constructor(props) {
@@ -13,70 +32,77 @@ class App extends Component {
       project: '',
       otReason: '',
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+  // handleInputChange(event) {
+  //   const target = event.target;
+  //   const value = target.value;
+  //   const name = target.name;
 
-    this.setState({
-      [name]: value
-    });
-  }
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // }
 
-  handleSubmit(event) {
-    axios.post(
-      'ot_record',
-      this.state,
-    ).then((response) => {
-      if(response.status === 200) {
-        alert('工时记录提交成功');
-      } else {
-        alert('工时记录提交失败');
-      }
-    })
-    .catch((error) => {
-      alert('工时记录提交失败');
-    });
-    event.preventDefault();
-  }
+  // handleSubmit(event) {
+  //   axios.post(
+  //     'ot_record',
+  //     this.state,
+  //   ).then((response) => {
+  //     if(response.status === 200) {
+  //       alert('工时记录提交成功');
+  //     } else {
+  //       alert('工时记录提交失败');
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     alert('工时记录提交失败');
+  //   });
+  //   event.preventDefault();
+  // }
 
   render() {
     return (
-      <div className="App">
-        <Form onSubmit={this.handleSubmit}>
-          <FormGroup>
-            <Label for="userName">姓名</Label>
-            <Input type="text" name="userName" id="userName" onChange={this.handleInputChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="department">部门</Label>
-            <Input type="text" name="department" id="department" onChange={this.handleInputChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="otDate">日期</Label>
-            <Input type="date" name="otDate" id="otDate" onChange={this.handleInputChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="otDuration">时数</Label>
-            <Input type="number" step="0.1" name="otDuration" id="otDuration" onChange={this.handleInputChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="project">所属项目</Label>
-            <Input type="text" name="project" id="project" onChange={this.handleInputChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="otReason">工作内容</Label>
-            <Input type="text" name="otReason" id="otReason" onChange={this.handleInputChange} />
-          </FormGroup>
-          
-          <Button >提交</Button>
-        </Form>
+      
+      <Router>
+        <div className="App">
+          <Navbar color="light" light expand="md">
+            <NavbarBrand><Link to="/">工时统计</Link></NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">提交工时记录</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">查看记录</NavLink>
+                </NavItem>
+                {/* <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown> */}
+              </Nav>
+            </Collapse>
+          </Navbar>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
 
-      </div>
     );
   }
 
