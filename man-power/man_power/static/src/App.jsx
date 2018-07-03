543a0c3a9944b765a330f8d19b096237667ba1be
yanglinzhen <yanglinzhen@govnet.com.cn>
2018-07-03 17:53:40 +0800

    add Demo page

--- man-power/man_power/static/src/App.jsx
+++ man-power/man_power/static/src/App.jsx
@@ -18,66 +18,32 @@ import { Button,
   DropdownItem
 } from 'reactstrap';
 import axios from './AxiosConfig.js'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, IndexRedirect } from "react-router-dom";
 import Home from './Home.jsx'
+import Demo from './Demo.jsx'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      userName: '',
-      department: '',
-      otDate: Date.now(),
-      otDuration: 0,
-      project: '',
-      otReason: '',
-    };
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    // this.handleInputChange = this.handleInputChange.bind(this);
+    
   }
-
-  // handleInputChange(event) {
-  //   const target = event.target;
-  //   const value = target.value;
-  //   const name = target.name;
-
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // }
-
-  // handleSubmit(event) {
-  //   axios.post(
-  //     'ot_record',
-  //     this.state,
-  //   ).then((response) => {
-  //     if(response.status === 200) {
-  //       alert('工时记录提交成功');
-  //     } else {
-  //       alert('工时记录提交失败');
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     alert('工时记录提交失败');
-  //   });
-  //   event.preventDefault();
-  // }
-
   render() {
     return (
-      
       <Router>
         <div className="App">
           <Navbar color="light" light expand="md">
             <NavbarBrand><Link to="/">工时统计</Link></NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
+            <Collapse isOpen={true} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
                   <NavLink href="/components/">提交工时记录</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink href="https://github.com/reactstrap/reactstrap">查看记录</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink><Link to="/demo">Demo</Link></NavLink>
                 </NavItem>
                 {/* <UncontrolledDropdown nav inNavbar>
                   <DropdownToggle nav caret>
@@ -100,6 +66,7 @@ class App extends Component {
             </Collapse>
           </Navbar>
           <Route exact path="/" component={Home} />
+          <Route exact path="/demo" component={Demo} />
         </div>
       </Router>
 
